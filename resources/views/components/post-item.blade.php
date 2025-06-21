6bb3b7b907b77ee1d09c92f31ee9ea7434066bce
suman khatiwada Github <sumankhatiwada786@gmail.com>
2025-06-21 23:54:55 +0545

    change index page style

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,22 +1,22 @@
 <div class="flex flex-col sm:flex-row bg-white rounded-lg shadow-md hover:shadow-lg transition duration-300 overflow-hidden">
-                    <div class="flex-1 p-4">
-                        <h5 class="text-lg font-bold text-gray-900 mb-2">{{ $post->title }}</h5>
-                        <p class="text-sm text-gray-700 mb-4">
-                            {{ Str::limit($post->content, 80) }}
-                        </p>
-                        <a href="#"
-                           class="inline-flex items-center text-sm text-blue-700 hover:underline font-medium">
-                            <x-primary-button>
-                                Readmore
-                                </x-primary-button>
-                            <svg class="ml-2 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                 viewBox="0 0 14 10">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                      stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
-                    </div>
-                   
-                        <img class="object-cover w-1p0 h-100" src="https://flowbite.com/docs/images/blog/image-1.jpg" alt="Post Image">
-                   
-                </div>
+    <div class="flex-1 p-4 flex flex-col justify-between">
+        <div>
+            <h5 class="text-lg font-bold text-gray-900 mb-2">{{ $post->title }}</h5>
+            <p class="text-sm text-gray-700 mb-4">
+                {{ Str::limit($post->content, 80) }}
+            </p>
+        </div>
+        <a href="#"
+           class="inline-flex items-center text-sm text-blue-700 hover:underline font-medium mt-2">
+            <x-primary-button>
+                Readmore
+            </x-primary-button>
+            <svg class="ml-2 w-4 h-4" xmlns="http://www.w3.org/2000/svg" fill="none"
+                 viewBox="0 0 14 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                      stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+            </svg>
+        </a>
+    </div>
+    <img class="object-cover w-48 h-48 sm:h-auto sm:w-48" src="{{ Storage::url($post->image) }}" alt="Post Image">
+</div>
